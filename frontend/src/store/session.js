249ab348aa8ce1527e01657e90a76733f08e258b
Taehoon Kim <taehoonk79@gmail.com>
2022-04-05 10:38:59 -0700

    authenticate me except modal

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,6 +1,86 @@
+import {csrfFetch} from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user)=>{
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+const removeUser = ()=>{
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const {credential, password} = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
+
+const initialState = {user: null};
+
+export const signup = (user) => async (dispatch) => {
+    const {first_name, last_name, email,password, zip_code} = user;
+    const response = await csrfFetch('/api/users',{
+        method: "POST",
+        body: JSON.stringify({
+            first_name,
+            last_name,
+            email,
+            password,
+            zip_code
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
+
+export function restoreUser() {
+    async function innerFn(dispatch) {
+        const response = await csrfFetch('/api/session');
+        const data = await response.json();
+        dispatch(setUser(data.user));
+        return response;
+    }
+    return innerFn; 
+}
+
+export const logout = () => async dispatch => {
+    const response = await csrfFetch('/api/session',{
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+}
 
 const sessionReducer = (state=initialState, action) => {
-
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({},state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({},state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
 }
 
 export default sessionReducer;
