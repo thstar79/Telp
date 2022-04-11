c263849672631621c870ec0b031a8508d08b46a6
thstar79 <taehoonk79@gmail.com>
2022-04-11 23:11:59 +0900

    heroku

--- frontend/src/components/MainRight/index.js
+++ frontend/src/components/MainRight/index.js
@@ -1,16 +1,16 @@
-import {Link} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 
 function MainRight(){
     return (
         <div>
-            <Link to="https://github.com/thstar79">
+            <Redirect to="https://github.com/thstar79">
                 <div>
                     <p>Taehoon Kim</p>
                     <div>
                         <img src="https://avatars.githubusercontent.com/u/50850699?v=4" width="150px" alt="No Img"/>
                     </div>
                 </div>
-            </Link>
+            </Redirect>
         </div>);
 }
 
