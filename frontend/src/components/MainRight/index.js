62157e9f1dd16fa5c074900849744160c32bf4c1
thstar79 <taehoonk79@gmail.com>
2022-04-12 00:06:45 +0900

    devleoper

--- frontend/src/components/MainRight/index.js
+++ frontend/src/components/MainRight/index.js
@@ -1,15 +1,9 @@
 function MainRight(){
     return (
         <div>
-            <a href="https://github.com/thstar79">
-                <div>
-                    <p>Taehoon Kim</p>
-                    <div>
-                        <img src="https://avatars.githubusercontent.com/u/50850699?v=4" width="50px" alt="No Img"/>
-                    </div>
-                </div>
-            </a>
-        </div>);
+          <a className="dev-link" href='https://github.com/thstar79/'>Developer</a>
+      </div>
+      );
 }
 
 export default MainRight;
