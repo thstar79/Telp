c17ac5d8891019bcc26a2718e1ef925cf2dfe1eb
thstar79 <taehoonk79@gmail.com>
2022-04-11 23:16:52 +0900

    miche

--- frontend/src/components/MainRight/index.js
+++ frontend/src/components/MainRight/index.js
@@ -1,16 +1,14 @@
-import {Redirect} from "react-router-dom";
-
 function MainRight(){
     return (
         <div>
-            <Redirect to="https://github.com/thstar79">
+            <a href="https://github.com/thstar79">
                 <div>
                     <p>Taehoon Kim</p>
                     <div>
                         <img src="https://avatars.githubusercontent.com/u/50850699?v=4" width="150px" alt="No Img"/>
                     </div>
                 </div>
-            </Redirect>
+            </a>
         </div>);
 }
 
